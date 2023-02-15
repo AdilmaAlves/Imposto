5dcb47688c275392f437ac1eb94af9b3ebca41cc
Adilma Alves <125414087+AdilmaAlves@users.noreply.github.com>
2023-02-14 21:23:31 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿double salario,valorImposto = 0;
+double dFaixa0 = 0;
+double dFaixa1 = 5;
+double dFaixa2 = 10;
+double dFaixa3 = 20;
+
+
+Console.WriteLine("Digite o valor do salário bruto: ");
+salario = double.Parse(Console.ReadLine());
+
+if(salario <= 900.00){   
+    valorImposto = (dFaixa0*(salario/100));
+}
+else if(salario > 900.00 && salario <= 1500.00){
+    valorImposto = ((salario/100)*dFaixa1);
+}
+else if(salario < 1500.00 && salario <= 2500.00){
+    Console.WriteLine("Faixa 3");
+   valorImposto = ((salario/100)*dFaixa2); 
+}
+else if(salario > 2500.00){
+   valorImposto = ((salario/100)*dFaixa3);
+}
+
+Console.WriteLine($"Valor que será cobrado de imposto: {valorImposto}");
